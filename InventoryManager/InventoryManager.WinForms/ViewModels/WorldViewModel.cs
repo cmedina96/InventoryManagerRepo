ff47c531296f5342de3b80938beca1f3929d73de
Cristian Medina <cmedina1996@knights.ucf.edu>
2019-11-03 15:43:16 -0500

    Winforms Data Binding Part 1
    Winforms Data Binding Part1 tutorial progress.

---
+++ InventoryManager/InventoryManager.WinForms/ViewModels/WorldViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using InventoryManager.Data;
+
+namespace InventoryManager.WinForms.ViewModels
+{
+    // Encapsulates a World
+    public class WorldViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Filename { get; set; }
+        public World World { get; set; }
+    }
+}
