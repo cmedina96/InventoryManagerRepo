967f83a4231f9c5c6d43b9ad155ad9f454dcd020
Cristian Medina <cmedina1996@knights.ucf.edu>
2019-11-05 23:41:39 -0500

    Revert "Initial commit"
    This reverts commit 0c79ef321e59fc96fe9cf4efa6ff3f5fad95bf02.

---
+++ InventoryManager/InventoryManager.Data/World.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace InventoryManager.Data
+{
+    public class World : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public List<Player> Players { get; set; }
+        public List<Item> Items { get; set; }
+        public World()
+        {
+            Players = new List<Player>();
+            Items = new List<Item>();
+        }
+    }
+}
